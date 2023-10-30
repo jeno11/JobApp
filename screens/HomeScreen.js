eb5e02ae363a5a2c5dc830658c810c35b522f1f2
jeno11 <jenoshavisakarasa11@gmail.com>
2023-10-31 00:10:56 +0530

    Home screen design initialised

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,16 @@
-import { StyleSheet, Text, TouchableOpacity, View, ScrollView, ImageBackground } from 'react-native'
+import { 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
+  View, 
+  ScrollView, 
+  ImageBackground, 
+  SafeAreaView } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
+import Icon from '@expo/vector-icons/MaterialCommunityIcons'
 import { useNavigation } from '@react-navigation/native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const HomeScreen = () => {
     const navigation = useNavigation()
@@ -16,14 +25,27 @@ const HomeScreen = () => {
     }
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ImageBackground source={require('../assets/bg7.jpg')} resizeMode='cover' style={styles.image} opacity={0.7}>
-        <Text style={styles.text}>Email: {auth.currentUser?.email}</Text>
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      <ImageBackground 
+        source={require('../assets/bg7.jpg')} 
+        //resizeMode='stretch' 
+        style={styles.image} 
+        opacity={0.7}
+      >
+        <View style={styles.top}>
+          <Icon name='menu' size={30} color='#a2a2db' style={{ width: 20 }}/>
+          <Icon name='account-circle' size={33} color='#a2a2db' style={{ marginLeft: 270 }}/>
+        </View>
+
+        <View style={{ alignItems: 'center', marginTop: 20}}>
+          <Text style={styles.text}> Welcome </Text>
+        </View>
+        {/* <Text style={styles.text}>Email: {auth.currentUser?.email}</Text>
         <TouchableOpacity style={styles.button} onPress={handleSignOut}>
           <Text style={styles.buttonText}>Sign Out</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </ImageBackground>
-    </ScrollView>
+    </KeyboardAwareScrollView>
   )
 }
 
@@ -32,19 +54,28 @@ export default HomeScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        // justifyContent: 'center',
+        // alignItems: 'center'
     },
     image: {
       flex: 1,
-      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+      //justifyContent: 'center',
       backgroundColor: '#000000'
     },
     text: {
     color: 'black',
     fontWeight: '400',
-    fontSize: 16,
-    textAlign: 'center'
+    fontSize: 40,
+    fontFamily: 'BreeSerif-Regular'
+    },
+    top: {
+      flexDirection: 'row',
+      marginTop: 50,
+      //marginBottom: 680,
+      //alignItems: 'center',
+      paddingHorizontal: 20
     },
     button: {
         backgroundColor: '#afeeee',
